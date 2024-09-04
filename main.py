60c1573eb145e86bc897e598f127eade9b1ad8fc
theboban5 <108588821+theboban5@users.noreply.github.com>
2024-09-05 00:10:33 +0700

    set up skeleton about page

--- main.py
+++ main.py
@@ -60,5 +60,9 @@ for city in cities_list:
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
