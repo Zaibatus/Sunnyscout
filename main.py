d49d6858532009b186a54cb3e201d642dd9b7445
Paolo <paolosalvetti34@gmail.com>
2024-09-05 10:14:05 +0200

    Update coordinates in cvs

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template, redirect, url_for, request
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
 import pandas as pd
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
 import requests
 
 # Constants
@@ -15,47 +18,66 @@ MY_LONG = 10.278610
 app = Flask(__name__)
 
 
-# Data retrieval functions
-def get_cities_data():
+#Update coordinates in CVS file
+def update_coordinates():
     df = pd.read_csv('cities_airports.csv', usecols=['City'])
-    return df['City'].tolist()
+    cities_list = df['City'].tolist()
 
-
-#coordinates API function
-def get_coordinates():
-    cities_list = get_cities_data()
     city_coordinates = {}
+
     for city in cities_list:
         city_params = {
             "q": city,
             "appid": OPEN_WEATHER_API_KEY,
             "limit": 1
         }
-        response = requests.get("http://api.openweathermap.org/geo/1.0/direct", params=city_params)
-        response.raise_for_status()
-        data = response.json()
+        try:
+            response = requests.get("http://api.openweathermap.org/geo/1.0/direct", params=city_params)
+            response.raise_for_status()
+            data = response.json()
 
-        if data:
-            city_coordinates[city] = {
-                "lat": data[0]["lat"],
-                "lon": data[0]["lon"]
-            }
-        else:
-            print(f"No coordinates found for {city}")
+            # If data is available, extract latitude and longitude
+            if data:
+                city_coordinates[city] = {
+                    "lat": data[0]["lat"],
+                    "lon": data[0]["lon"]
+                }
+            else:
+                print(f"No coordinates found for {city}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data for {city}: {e}")
 
-    return city_coordinates
+    df['Latitude'] = df['City'].map(lambda x: city_coordinates.get(x, {}).get('lat'))
+    df['Longitude'] = df['City'].map(lambda x: city_coordinates.get(x, {}).get('lon'))
+
+    df.to_csv('cities_airports.csv', index=False)
 
 
-def get_weather():
-    weather_params = {
-        "lat": MY_LAT,
-        "lon": MY_LONG,
-        "appid": OPEN_WEATHER_API_KEY,
-        "cnt": 16,
+def get_sunshine_forecast(city, lat, lon):
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "daily": "sunshine_duration",
+        "timezone": "GMT",
+        "forecast_days": 16
     }
-    response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=weather_params)
-    response.raise_for_status()
-    return response.json()
+
+    response = openmeteo.weather_api(url, params=params)[0]
+
+    daily = response.Daily()
+    daily_sunshine_duration = daily.Variables(0).ValuesAsNumpy()
+
+    daily_data = {"date": pd.date_range(
+        start=pd.to_datetime(daily.Time(), unit="s", utc=True),
+        end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
+        freq=pd.Timedelta(seconds=daily.Interval()),
+        inclusive="left"
+    ), "sunshine_duration": daily_sunshine_duration / 3600}
+
+    return pd.DataFrame(data=daily_data)
+
+
 
 # Routes
 @app.route("/")
@@ -70,5 +92,4 @@ def about():
 
 # Main execution
 if __name__ == '__main__':
-    print(get_coordinates())
     app.run(host='0.0.0.0', port=5001, debug=True)
