bb5a11e57350ce57093f871caf7b725050132fb0
theboban5 <108588821+theboban5@users.noreply.github.com>
2024-09-23 23:30:41 +0700

    merge images to cities_airports

--- csv_merge.py
+++ csv_merge.py
@@ -4,19 +4,14 @@ import pandas as pd
 original_csv = pd.read_csv('cities_airports.csv')
 
 # Load the new data (the carousel image CSV)
-new_data = pd.read_csv('carousel_images_cities.csv')
+new_data = pd.read_csv('carousel_images.csv')
 
 # Merge the data
 # Assuming 'City' is the common identifier in both CSVs
-merged_data = pd.merge(original_csv, new_data, on='City', how='left')
+merged_data = pd.merge(original_csv, new_data[['City', 'city_img_0']], on='City', how='left')
 
 # Fill NaN values in the new columns with appropriate defaults if needed
-# Adjust column names as necessary
-merged_data['city_img_1'].fillna('https://example.com/default-image.jpg', inplace=True)
-merged_data['city_img_2'].fillna('https://example.com/default-image.jpg', inplace=True)
-merged_data['city_img_3'].fillna('https://example.com/default-image.jpg', inplace=True)
-merged_data['city_img_4'].fillna('https://example.com/default-image.jpg', inplace=True)
-
+merged_data['city_img_0'].fillna('https://example.com/default-image.jpg', inplace=True)
 
 # Save the merged data back to a CSV file
 merged_data.to_csv('cities_airports.csv', index=False)
